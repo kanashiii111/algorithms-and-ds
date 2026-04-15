986262dc4b9dcaa51315f5e6a68b05384382534b
kanashiii111 <FrageSK@yandex.ru>
2026-04-16 00:24:04 +0300

    huh

--- lab1/debut.py
+++ lab1/debut.py
@@ -11,17 +11,34 @@ as_black: bool = False
 ep_cell = ""
 castle = []
 
-queens_gambit = {
+white = {
     1: "d2d4",
     2: "c2c4",                    
     3: "g1f3",                   
     4: "b1c3",
     5: "c1f4",
     6: "e2e3",
-    7: "f1d3",
+    7: "f1e2",
     8: "e1g1",
 }
-    
+
+black = {
+    1: "g1f3",
+    2: "d2d3",
+    3: "g2g3",
+    4: "f1g2",
+    5: "e1g1",
+    6: "b1d2",
+    7: "e2e4",
+    8: "f1e1"
+}
+
+def is_valid_move(board, move) -> bool:
+    from_x, from_y = ord(move[0]) - ord('a'), int(move[1]) - 1
+    to_x, to_y = ord(move[2]) - ord('a'), int(move[3]) - 1
+    if board[to_x][to_y][0] == True and board[to_x][to_y][1] != EMPTY: return False
+    return True
+
 def read_board():
     global as_black, as_white, ep_cell, castle
 
@@ -36,14 +53,10 @@ def read_board():
         x, y = ord(pos[0]) - ord('a'), int(pos[1]) - 1
 
         is_my_team = line[1] == '+'
-        if is_my_team:
-            if y <= 1:
-                as_white = True
-            else:
-                as_black = True
 
         type = int(line[2])
         board[x][y] = [is_my_team, type]
+
     ep_cell = input()
     castle = [x for x in input().split()]
 
@@ -54,12 +67,17 @@ def read_board():
 # ========================
 
 def main():
+    global move_history, check
     board, ping = read_board()
     move_number = ping + 1
 
-    move = queens_gambit[move_number] 
-
-    print(f"{move} {move_number}")
+    move = black[move_number] 
+    check += "q"   
+    
+    if is_valid_move(board, move):
+        move_history.append(move)
+        print(f"{move} {move_number}")
+        print(f"# {move} {move_number}")
 
 if __name__ == "__main__":
     main()
